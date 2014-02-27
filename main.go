4c4e33d812b699498e8fee5eb0f4dca098b3790a
jake <jake@coffshire.com>
2014-02-26 20:31:49 -0600

    init

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// error response contains everything we need to use http.Error
+type handlerError struct {
+	Error   error
+	Message string
+	Code    int
+}
+
+// a custom type that we can use for handling errors and formatting responses
+type handler func(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError)
+
+// attach the standard ServeHTTP method to our handler so the http library can call it
+func (fn handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// here we could do some prep work before calling the handler if we wanted to
+
+	// call the actual handler
+	response, err := fn(w, r)
+
+	// check for errors
+	if err != nil {
+		log.Printf("ERROR: %v\n", err.Error)
+		http.Error(w, fmt.Sprintf(`{"error":"%s"}`, err.Message), err.Code)
+		return
+	}
+	if response == nil {
+		log.Printf("ERROR: response from method is nil\n")
+		http.Error(w, "Internal server error. Check the logs.", http.StatusInternalServerError)
+		return
+	}
+
+	// turn the response into JSON
+	bytes, e := json.Marshal(response)
+	if e != nil {
+		http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// send the response and log
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(bytes)
+	log.Printf("%s %s %s %d", r.RemoteAddr, r.Method, r.URL, 200)
+}
+
+func appHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	// command line flags
+	port := flag.Int("port", 80, "port to serve on")
+	dir := flag.String("directory", "static/", "directory of web files")
+	flag.Parse()
+
+	// handle all requests by serving a file of the same name
+	fs := http.Dir(*dir)
+	fileHandler := http.FileServer(fs)
+
+	// setup routes
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
+	})
+	// REST routes go here
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static", fileHandler))
+	http.Handle("/", router)
+
+	log.Printf("Running on port %d\n", *port)
+
+	addr := fmt.Sprintf("127.0.0.1:%d", *port)
+	// this call blocks -- the progam runs here forever
+	err := http.ListenAndServe(addr, nil)
+	fmt.Println(err.Error())
+}
